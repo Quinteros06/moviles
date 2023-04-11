d523a51ee7e5d83db074e5cb8e506abc40ed20f6
Quinteros06 <00220619@uca.edu.sv>
2023-04-11 16:07:17 -0600

    laboratorio 4

---
+++ laboratorio04/app/src/main/java/com/example/laboratorio04/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.laboratorio04
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.Placeholder
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextName: TextInputEditText
+    private lateinit var editTextEmail: TextInputEditText
+    private lateinit var editTextNumer: TextInputEditText
+    private lateinit var buttonsave: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editTextName = findViewById(R.id.namee)
+        editTextEmail = findViewById(R.id.emaill)
+        editTextNumer = findViewById(R.id.numeroo)
+
+        buttonsave.setOnClickListener{
+            val intent = Intent(this, secundario:: class.java)
+            intent.putExtra("name", editTextName.text)
+            intent.putExtra("Email", editTextEmail.text)
+            intent.putExtra("Número", editTextNumer.text)
+            startActivity(intent)
+        }
+    }
+}
---
+++ laboratorio04/app/src/main/java/com/example/laboratorio04/secundario.kt
@@ -0,0 +1,12 @@
+package com.example.laboratorio04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secundario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secundario)
+
+    }
+}
