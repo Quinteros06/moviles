e3539a0261d745c93028cd270eedc6d76b59aa17
Quinteros06 <00220619@uca.edu.sv>
2023-04-18 20:18:03 -0600

    falta text

--- laboratorio05/app/src/main/java/com/example/laboratorio_05/primeroFragment.kt
+++ laboratorio05/app/src/main/java/com/example/laboratorio_05/primeroFragment.kt
@@ -5,55 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [primeroFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class primeroFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var CardView: CardView
+    private lateinit var Button: Button
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedState: Bundle?
+    ): View{
+        return inflater.inflate(R.layout.fragment_primero,container, false)
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        bindFloating()
+        bindCard()
+        bindButton()
+        floatingActionButton.setOnClickListener{
+            it.findNavController().navigate(R.id.action_primeroFragment_to_segundoFragment)
+        }
+
+        CardView.setOnClickListener{
+            it.findNavController().navigate(R.id.action_primeroFragment_to_segundoFragment)
+        }
+
+        Button.setOnClickListener{
+            it.findNavController().navigate(R.id.action_primeroFragment_to_segundoFragment)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_primero, container, false)
+    private fun bindFloating(){
+        floatingActionButton = view?.findViewById(R.id.floatingActionButton) as FloatingActionButton
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment primeroFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            primeroFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun bindCard(){
+        CardView = view?.findViewById(R.id.cardView) as CardView
+    }
+
+    private fun bindButton(){
+        Button = view?.findViewById(R.id.button) as Button
     }
 }
